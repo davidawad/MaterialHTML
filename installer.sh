ce7435237e0611026a4f301fc9103dad717c9036
david <davidawad64@gmail.com>
2014-11-27 01:34:01 -0500

    added an animated backdrop

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
-echo this will install bower for you as well as necessary dependencies for web components, and then when done run the python script to launch the server.
+echo "this will install bower for you as well as necessary dependencies for web components, and then when done run the python script to launch the server."
 
 ls
 
-echo it's a shame i have no time so this is all that this does right now.
+echo "its a shame i have no time so this is all that this does right now."
 
 
 
 
-echo done. Check out the page at http://localhost:8000/index.html 
+echo "done. Check out the page at http://localhost:8000/index.html"
